f2759cf1cac167be95242652a067db1d4e464890
eimanesmp <eimanesmp@gmail.com>
2025-03-10 14:46:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// Abrir aba
+document.getElementById('BotaoBarraLateralAbrir').addEventListener('click', () => {
+    document.getElementById('BarraLateral').classList.toggle('aberta')
+});
+document.getElementById('BarraLateral').addEventListener('mouseenter', () => {
+    document.getElementById('BarraLateral').classList.add('aberta')
+});
+document.getElementById('BarraLateral').addEventListener('mouseleave', () => {
+    document.getElementById('BarraLateral').classList.remove('aberta')
+});
+
+// Selecionar uma aba
+const Abas = Array.from(document.getElementsByClassName('BarraLateralItem')).filter(aba => aba.onclick == null);
+const CorpoAbas = Array.from(document.getElementsByClassName('CorpoAba'));
+Abas.forEach(aba => {
+    aba.addEventListener('click', () => {
+
+        Abas.forEach((aba2) => {
+            if (aba2.classList.contains('ativo')) {
+                aba2.classList.remove('ativo');
+            };
+        });
+
+        CorpoAbas.forEach((aba2) => {
+            if (aba2.classList.contains('ativo')) {
+                aba2.classList.remove('ativo');
+            };
+        });
+
+        aba.classList.add('ativo');
+        CorpoAbas.find(caba => caba.id.replace("Corpo","Lateral") == aba.children.namedItem('span').id).classList.add('ativo');
+
+    });
+});
+
+// Aba pelo link
+let AbaPadrao = Abas.find(aba => window.location.hash.replace("#","").startsWith(aba.children.namedItem('span').id.replace("LateralAba","")));
+if(AbaPadrao) {
+    let aba = AbaPadrao;
+    aba.classList.add('ativo');
+    CorpoAbas.find(caba => caba.id.replace("Corpo","Lateral") == aba.children.namedItem('span').id).classList.add('ativo');
+} else {
+    let aba = Abas.find(aba => aba.children.namedItem('span').id == "LateralAbaInicio");
+    aba.classList.add('ativo');
+    CorpoAbas.find(caba => caba.id.replace("Corpo","Lateral") == aba.children.namedItem('span').id).classList.add('ativo');
+}
