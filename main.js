df79bb5acbe1aa82bd00978476dadb3fa7498d5a
eimanesmp <eimanesmp@gmail.com>
2025-04-27 15:41:36 -0300

    Sem hashing

--- main.js
+++ main.js
@@ -1,39 +1,4 @@
-// Selecionar uma aba
-const Abas = Array.from(document.getElementsByClassName('BarraLateralItem')).filter(aba => aba.onclick == null && aba.id != "AbaIdMapa");
-const CorpoAbas = Array.from(document.getElementsByClassName('CorpoAba'));
-Abas.forEach(aba => {
-    aba.addEventListener('click', () => {
-
-        Abas.forEach((aba2) => {
-            if (aba2.classList.contains('ativo')) {
-                aba2.classList.remove('ativo');
-            };
-        });
-
-        CorpoAbas.forEach((aba2) => {
-            if (aba2.classList.contains('ativo')) {
-                aba2.classList.remove('ativo');
-            };
-        });
-
-        aba.classList.add('ativo');
-        CorpoAbas.find(caba => caba.id.replace("Corpo","Lateral") == aba.children.namedItem('span').id).classList.add('ativo');
-
-    });
-});
-
-// Aba pelo link
-let AbaPadrao = Abas.find(aba => window.location.hash.replace("#","").startsWith(aba.children.namedItem('span').id.replace("LateralAba","")));
-if(AbaPadrao) {
-    let aba = AbaPadrao;
-    aba.classList.add('ativo');
-    CorpoAbas.find(caba => caba.id.replace("Corpo","Lateral") == aba.children.namedItem('span').id).classList.add('ativo');
-} else {
-    let aba = Abas.find(aba => aba.children.namedItem('span').id == "LateralAbainicio" && aba.id != "AbaIdMapa");
-    aba.classList.add('ativo');
-    CorpoAbas.find(caba => caba.id.replace("Corpo","Lateral") == aba.children.namedItem('span').id).classList.add('ativo');
-}
-
-if(window.location.hash == "#mapa") {
+// Mapa externo
+if(window.location.href == "https://eimanesmp.shop/guia") {
     window.location = 'http://mapa.eimanesmp.shop:11004';
-}
+};
